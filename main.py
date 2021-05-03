1c80eb2964bbbd3b02f58aec6c90f87fb0246669
pakt <pakt@debian.home>
2021-05-03 10:39:10 -0500

    added error support, shell change support, and some other things

--- main.py
+++ main.py
@@ -4,20 +4,30 @@ from utils import check_username, create_user
 
 app = Flask(__name__)
 
+app.secret_key = "you'llneverguessthis;)"
+
+with open("/etc/shells", "r") as temp:
+    shells = temp.read().rstrip().split("\n")
+    del shells[0]
+
 @app.route("/")
 def root():
-    return render_template("index.html")
-    
+    return render_template("index.html", shells=shells)
     
 @app.route("/create", methods=["POST"])
 def create():
     user = request.form.to_dict()
-    if not check_username(user['username']):
-        return
+    check = check_username(user['username'])
+    if not check[0]:
+        flash("There has been an error with your application")
+        flash(check[1])
     else:
-        print(user)
-        create_user(user['username'], user['password'], user['sshkey'])
-        return redirect("/thanks")
+        created_user = create_user(user['username'], user['password'], user['sshkey'], user['shell'])
+        if not created_user[0]:
+            flash("There has been an error with your application")
+            flash(created_user[1])
+            
+    return redirect("/thanks")
     
 @app.route("/thanks")
 def thanks():
--- utils.py
+++ utils.py
@@ -3,22 +3,29 @@ import crypt
 import re
 
 def check_username(username):
-    if not re.match(r"[^a-zA-Z0-9]", username):
+    if re.match(r"^[a-zA-Z]+$", username):
         return True
     else:
-        return False
+        return (False, "Username Check Failed")
     
-def create_user(username, password, ssh_key):
+def create_user(username, password, ssh_key, shell):
     try:
         subprocess.call(["sudo", "useradd", "-m", username, "-p", crypt.crypt(password)])
     except subprocess.CalledProcessError:
-        return False
+        return (False, "Unknown Error")
     
     try:
         subprocess.call(["sudo", "mkdir", f"/home/{username}/.ssh"])
     except subprocess.CalledProcessError:
-        return False
+        return (False, "Unknown Error")
     
-    subprocess.call(["sudo", "echo", ssh_key, f"/home/{username}/.ssh/authorized_keys"])
-
+    try:
+        subprocess.call(["sudo", "echo", ssh_key, f"/home/{username}/.ssh/authorized_keys"])
+    except subprocess.CalledProcessError:
+        return (False, "Unknown Error")
+    
+    try:
+        subprocess.call(["sudo", "chsh", "--shell", shell, username])
+    except subprocess.CalledProcessError():
+        return (False, "Unknown Error")
     return True
